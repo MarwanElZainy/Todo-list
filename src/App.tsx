1ea29a8ddc69ed3ef203f99f2b1fbabd85aa7328
Marwan_ElZainy <marwan.elzainy@gmail.com>
2022-10-20 21:21:17 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef, useState} from 'react';
 import './App.css';
+import TodoItem from "./TodoItem";
 
 function App() {
+  const todoInputRef = useRef<HTMLInputElement>(null)
+
+  const [items, setItems] = useState([{id: 0, text: 'Task 1', completed: true}, {id: 1, text: 'Task 2', completed: false}])
+
+  function handleTodo() {
+    const todo = todoInputRef.current!.value
+    if (todo === '') return;
+    todoInputRef.current!.value = ''
+
+    setItems((prevState) => [...prevState, {id: prevState.length>0? (prevState[prevState.length-1].id +1):0 , text: todo, completed:false}])
+    console.log(items)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>TODO-ify</h1>
+
+        <div >
+          <h1>Add Item</h1>
+          <input ref={todoInputRef} type="text"/>
+          <button type={"submit"} onClick={handleTodo}>Add</button>
+        </div>
+
+        <div >
+          <h1>TODO LIST</h1>
+          <div id='todoList'>
+            {items.filter(item => !item.completed).map(item => <TodoItem key={item.id} item={item} setItems={setItems}/>)}
+          </div>
+        </div>
+
+        <div >
+          <h1>Completed</h1>
+          <div>
+            {items.filter(item => item.completed).map(item => <TodoItem key={item.id} item={item} setItems={setItems}/>)}
+          </div>
+        </div>
+      </div>
   );
 }
 
